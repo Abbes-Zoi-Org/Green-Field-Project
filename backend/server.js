e19b57f43d34f26656564adfea2773e9f7a5814f
Zoi Papamichail <109663228+zopapami@users.noreply.github.com>
2023-01-23 04:43:36 +0200

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,34 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const cors = require("cors");
 const users = require("./routes/api/users");
 const app = express();
-const cors = require("cors")
+
 app.use(
   bodyParser.urlencoded({
     extended: false,
   })
 );
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5001" }));
 
-const db = require("./config/keys").mongoURI;
-
-mongoose
-  .set("strictQuery", false)
-  .connect(db, { useNewUrlParser: true })
+const db = require("./models");
+db.mongoose
+  .connect(db.url, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
   .catch((err) => console.log(err));
 
 app.use(passport.initialize());
-require("./config/passport")(passport);
+require("./config/passport.js")(passport);
+
 app.use("/api/users", users);
+require("./routes/api/meal.routes")(app);
+
+// for testing
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the application." });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
