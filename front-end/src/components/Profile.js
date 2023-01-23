1654c2a627af6e150b35caaa37e19ad533327bcc
brokaX <broka_exogene@live.fr>
2023-01-24 01:06:50 +0200

    Added logged in  user details to local storage

--- front-end/src/components/Profile.js
+++ front-end/src/components/Profile.js
@@ -1,16 +1,16 @@
 import React from "react";
 // Services
-// import AuthService from "../services/AuthService.js";
+import AuthService from "../services/AuthService.js";
 
 const Profile = () => {
-  // const currentUser = AuthService.getCurrentUser();
-  const currentUser = {
-    username: "Zoi",
-    age: "29",
-    gender: "Female",
-    height: 160,
-    weight: 65
-  }
+  const currentUser = AuthService.getCurrentUser();
+  // const currentUser = {
+  //   username: "Zoi",
+  //   age: "29",
+  //   gender: "Female",
+  //   height: 160,
+  //   weight: 65
+  // }
 
   return (
     <div className="card-container card">
@@ -18,43 +18,33 @@ const Profile = () => {
         <h2 className="text-center">Profile</h2>
       </header>
       <br></br>
-        <div className="d-flex justify-content-evenly">
+      <div className="d-flex justify-content-evenly">
         <div>
-        <p>
-          <strong>Name:</strong>
-        </p>
-        <p>
-          <strong>Age:</strong>
-        </p>
-        <p>
-          <strong>Gender:</strong>
-        </p>
-        <p>
-          <strong>Height:</strong>
-        </p>
-        <p>
-          <strong>Weight:</strong>
-        </p>
+          <p>
+            <strong>Name:</strong>
+          </p>
+          <p>
+            <strong>Age:</strong>
+          </p>
+          <p>
+            <strong>Gender:</strong>
+          </p>
+          <p>
+            <strong>Height:</strong>
+          </p>
+          <p>
+            <strong>Weight:</strong>
+          </p>
         </div>
         <div>
-        <p>
-          {currentUser.username}
-        </p>
-        <p>
-          {currentUser.age}
-        </p>
-        <p>
-          {currentUser.gender}
-        </p>
-        <p>
-          {currentUser.height} cm^2
-        </p>
-        <p>
-          {currentUser.weight} kg
-        </p>
+          <p>{currentUser.user.name}</p>
+          <p>{currentUser.user.age}</p>
+          <p>{currentUser.user.gender}</p>
+          {currentUser.user.height} cm<sup>2</sup>
+          <p></p>
+          <p>{currentUser.user.weight} kg</p>
         </div>
-        </div>
-
+      </div>
     </div>
   );
 };
